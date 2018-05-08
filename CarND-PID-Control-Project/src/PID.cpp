648ee443ad961c85967f125471bbcdd0e10803ce
Min-Jung Wang <nonlining@gmail.com>
2018-05-08 14:51:51 +0800

    update

--- CarND-PID-Control-Project/src/PID.cpp
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -22,10 +22,6 @@ void PID::Init(double Kp, double Ki, double Kd) {
 void PID::UpdateError(double cte) {
 	
 	this->sum += cte;
-
-    //this->p_error = - Kp * cte;
-    //this->i_error = - Ki * sum;
-    //this->d_error = - Kd * (cte - prev);
 	this->p_error = - cte;
     this->i_error = - sum;
     this->d_error = - (cte - prev);
@@ -33,7 +29,6 @@ void PID::UpdateError(double cte) {
 }
 
 double PID::TotalError() {
-    //return this->p_error + this->i_error + this->d_error;
 	return Kp * this->p_error + Ki * this->i_error + Kd * this->d_error;
 }
 
