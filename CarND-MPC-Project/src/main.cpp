a5891290d3a4234b7c778cf56972427dcef88e60
Min-Jung Wang <nonlining@gmail.com>
2018-05-14 18:27:58 +0800

    update

--- CarND-MPC-Project/src/main.cpp
+++ CarND-MPC-Project/src/main.cpp
@@ -95,7 +95,8 @@ int main() {
           double px = j[1]["x"];
           double py = j[1]["y"];
           double psi = j[1]["psi"];
-          double v = j[1]["speed"] * 0.447;
+          double v1 = j[1]["speed"];
+		  double v = v1*0.447;
 		  double steering_angle = j[1]["steering_angle"];
 		  double throttle = j[1]["throttle"];
 
