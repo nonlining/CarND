4cd4a87e964353b245d3a5b61e7535c188c38ad4
Min-Jung Wang <nonlining@gmail.com>
2018-05-14 18:01:56 +0800

    update

--- CarND-MPC-Project/src/MPC.cpp
+++ CarND-MPC-Project/src/MPC.cpp
@@ -112,7 +112,7 @@ class FG_eval {
       fg[2 + psi_start + i] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
       fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
       fg[2 + cte_start + i] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
-      fg[2 + epsi_start + i] = epsi1 - ((psi0 - psides0) + v0 * delta0 / LF * dt);
+      fg[2 + epsi_start + i] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
     }
   }
 };
