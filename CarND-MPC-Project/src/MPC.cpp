84cf4f70ee4f81b70214edd713cf2019ebb9e860
Min-Jung Wang <nonlining@gmail.com>
2018-05-14 16:24:18 +0800

    update

--- CarND-MPC-Project/src/MPC.cpp
+++ CarND-MPC-Project/src/MPC.cpp
@@ -2,6 +2,7 @@
 #include <cppad/cppad.hpp>
 #include <cppad/ipopt/solve.hpp>
 #include "Eigen-3.3/Eigen/Core"
+#include <math.h>
 
 using CppAD::AD;
 
@@ -46,20 +47,23 @@ class FG_eval {
 
 
     for (int i = 0; i < N; i++) {
-      fg[0] += 3000*CppAD::pow(vars[cte_start + i], 2);
-      fg[0] += 3000*CppAD::pow(vars[epsi_start + i], 2);
-      fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
+      fg[0] += 3000*pow(vars[cte_start + i], 2);
+      //fg[0] += 3000*pow(vars[epsi_start + i], 2);
+	  fg[0] += 500*pow(vars[epsi_start + i], 2);
+      fg[0] += pow(vars[v_start + i] - ref_v, 2);
     }
 
     for (int i = 0; i < N - 1; i++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + i], 2);
-      fg[0] += 5*CppAD::pow(vars[a_start + i], 2);
-      fg[0] += 700*CppAD::pow(vars[delta_start + i] * vars[v_start+i], 2);
+      //fg[0] += 5*pow(vars[delta_start + i], 2);
+	  fg[0] += 1*pow(vars[delta_start + i], 2);
+      //fg[0] += 5*pow(vars[a_start + i], 2);
+	  fg[0] += 1*pow(vars[a_start + i], 2);
+      //fg[0] += 700*pow(vars[delta_start + i] * vars[v_start+i], 2);
     }
 
     for (int i = 0; i < N - 2; i++) {
-      fg[0] += 200*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
-      fg[0] += 10*CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+      fg[0] += 200*pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+      fg[0] += 1*pow(vars[a_start + i + 1] - vars[a_start + i], 2);
     }
 
     // initialize constraints
